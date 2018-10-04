1c6100d83854a0c4fa241d548bb4a3b6343b8732
Naomi <mangogo.naomi@qq.com>
2018-10-05 02:00:33 +0800

    update

--- 6-11 求自定类型元素序列的中位数/源.cpp
+++ 6-11 求自定类型元素序列的中位数/源.cpp
@@ -17,7 +17,40 @@ int main()
 
 	return 0;
 }
+//ElementType Median(ElementType A[], int N)
+//{
+//	ElementType t;
+//	//√∞≈›≈≈–Ú
+//	for (int i = 0; i < N - 1; i++)
+//	{
+//		for (int j = 0; j < N - i - 1; j++)
+//		{
+//			if (A[j] > A[j + 1])
+//			{
+//				t = A[j + 1];
+//				A[j + 1] = A[j];
+//				A[j] = t;
+//			}
+//		}
+//	}
+//	return A[N / 2 ];
+//}
 ElementType Median(ElementType A[], int N)
 {
-
+	int i, j, Increment;
+	ElementType Tmp;
+	//œ£∂˚≈≈–Ú
+	for (Increment = N / 2; Increment > 0; Increment /= 2) {
+		for (i = Increment; i < N; i++) {
+			Tmp = A[i];
+			for (j = i; j >= Increment; j -= Increment) {
+				if (Tmp < A[j - Increment])
+					A[j] = A[j - Increment];
+				else
+					break;
+			}
+			A[j] = Tmp;
+		}
+	}
+	return A[N / 2];
 }
